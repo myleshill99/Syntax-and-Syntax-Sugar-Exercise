65461277e542de16566d5c5fc7ae30567d8cfe79
Myles Hill <mylehill99@gmail.com>
2023-10-17 14:25:54 -0400

    finished

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -2,9 +2,23 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            // int answer = 4 // explcit typing 
+            // string response; //explicit typing 
+
+            //if (answer < 9) 
+            //{
+            // response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            // }
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+        }        
+        
     }
 }
